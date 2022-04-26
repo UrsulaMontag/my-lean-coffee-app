3e7e79401aa5f10732c65cbeb16ac399d8ba658e
Ursula Montag <montagu666@gmail.com>
2022-04-26 14:34:33 +0200

    Add mui library

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,33 @@
+import { AppBar, Button, Container, Toolbar } from "@mui/material";
 import Link from "next/link";
 
 export default function Header() {
+  return (
+    <AppBar>
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Link href="/" passHref>
+            <Button component="a" sx={{ color: "white" }}>
+              Home
+            </Button>
+          </Link>
+
+          <Link href="/cards" passHref>
+            <Button component="a" sx={{ color: "white" }}>
+              Cards
+            </Button>
+          </Link>
+
+          <Link href="/create" passHref>
+            <Button component="a" sx={{ color: "white" }}>
+              Create
+            </Button>
+          </Link>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+
   return (
     <header>
       <Link href="/">Home</Link>
@@ -8,7 +35,6 @@ export default function Header() {
       <Link href="/cards">Cards</Link>
       <br />
       <Link href="/create">Create</Link>
-      <br />
     </header>
   );
 }
